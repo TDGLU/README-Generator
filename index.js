bdedec39240d99d87a81df97daa8b8ee563eb973
TDGLU <tdglukealt@gmail.com>
2022-12-03 19:45:59 -0800

    added questions

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 const inquirer = require("inquirer");  
 
-// const questions = [
-//   {
-//     type: "input",
-//     message: "What is your GitHub username?",
-//     name: "Username",
-//   }
-// ];
+const questions = [
+  // Title
+  {
+    type: "input",
+    message: "What would you like your project title to be?",
+    name: "title"
+  },
+  // Description 
+  {
+    type: "input",
+    message: "What is the description of your project?",
+    name: "description"
+  },
+  // Installations 
+  {
+    type: "input",
+    message: "How could users install your project?",
+    name: "installations"
+  },
+  // Usage 
+  {
+    type: "input",
+    message: "How could users use your application?",
+    name: "usage"
+  },
+  // contribution 
+  {
+    type: "input",
+    message: "What are contribution rules for your project?",
+    name: "contribution"
+  },
+  // Test 
+  {
+    type: "input",
+    message: "How could users test your application?",
+    name: "test"
+  },
+];
 
 function init() {
   inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "what would you like your profile title to be?",
-      name: "title"
-    }
-  ])
+  .prompt(questions)
   .then((data) => {
-    console.log("The user's title is " + data.title)
+    // console.log("The user's title is " + data.title)
+    console.log(data)
   })
   .catch((error) => {
     console.log(error)
